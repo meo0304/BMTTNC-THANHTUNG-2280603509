53e592c4535d42d52903913c6446bc78ed8b1f05
Thanh Tùng <tungtan5792@gmail.com>
2025-05-28 14:10:39 +0700

    [ADD] TRANSPOSITION

--- BUOI_2/LAB_02/api.py
+++ BUOI_2/LAB_02/api.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from cipher.vigenere import VigenereCipher  # Thêm vào phần đầu của file api.py
 from cipher.railfence import RailFenceCipher # Thêm vào phần đầu của file api.py
 from cipher.playfair import PlayFairCipher
+from cipher.transposition import TranspositionCipher
 
 app = Flask(__name__)
 
@@ -96,6 +97,27 @@ def playfair_decrypt():
     decrypted_text = playfair_cipher.playfair_decrypt(cipher_text,playfair_matrix)
     return jsonify({'decrypted_text': decrypted_text})
 
+# Thêm đoạn sau vào trước hàm main
+#TRANSPOSITION CIPHER ALGORITHM
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data.get('plain_text')
+    key = int(data.get('key'))
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data.get('cipher_text')
+    key = int(data.get('key'))
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+    
 #main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
